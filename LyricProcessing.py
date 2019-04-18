f9280fcfd9aea173fa265d9b2ed8086eb232b818
alexander-schott <32785666+alexander-schott@users.noreply.github.com>
2019-04-18 11:38:28 -0400

    Commented LyricProcessing file.

--- LyricProcessing.py
+++ LyricProcessing.py
@@ -6,19 +6,26 @@ import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.tokenize import RegexpTokenizer
 
-# a list of sentences
+# TestData: a list of sentences that will be annotated for sentiment and phoneme count.
 testData = []
+# The testing data is read out of the file 'test_dat.txt'
 lyrics = open('test_data.txt', 'r')
 for line in lyrics:
     testData.append(line)
 
-
+"""
+    Helper function to sentence tokenize a paragraph.
+"""
 def paragraphToSentecnes(paragraph):
     return nltk.sent_tokenize(paragraph)
 
-#nltk.download('cmudict')
+"""
+ Seperates a sentence into phonemes using cmudict lexicon, and counts the number of phonemes.
+ Words not in the lexicon are assumed to be one phoneme.
+"""
 arpabet = nltk.corpus.cmudict.dict()
 def countPhonemes(sentence):
+    #split into words without punctuation
     tokenizer = RegexpTokenizer(r'\w+')
     tokens = tokenizer.tokenize(sentence)
     count = 0
@@ -29,11 +36,14 @@ def countPhonemes(sentence):
         except Exception as e:
             print(e)
             count += 1
-    print (count)
+    print(count)
     return count
 
-arousal_docs = []
-
+"""
+    Removes stop words, and lemmatizes words in order to remove noise from data 
+    and reduce the size of the number of values trained over.
+    Takes in a sentence as a string, and returns a list of words.
+"""
 def sentenceToFeatures(sentence):
     lem = WordNetLemmatizer()
     txt = []
@@ -47,6 +57,9 @@ def sentenceToFeatures(sentence):
             #dictionary.add(word)
     return txt
 
+"""
+    Returns an emotion value based on a valence and arousal score.
+"""
 def categorizeSentiment(v, a):
     sent = ""
     if v <= 0 and a <= 0:
@@ -60,44 +73,62 @@ def categorizeSentiment(v, a):
     return sent
 
 print("Reading data...")
+# The emobank dataset contains text entries scored for valence, arousal, and dominance.
+# Arousal values will be used to train the classifier
 eb = pd.read_csv('emobank.csv', index_col=0)
+
+# Data is read out of emobank and stored in a useful way in arousal_docs
+arousal_docs = []
 
 
 #read emobank
 for index, row in eb.iterrows():
     a = float(row["A"])
     text = row["text"]
-    # change from 6 pt positive scale to range[-1, 1]
+    # transform arousal value from 6 pt positive scale to range[-1, 1]
     a = a * 2 / 6 - 1
     try:
+        # lemmatize, remove stop words, and transform sentence to list of words to build training docs
         txt = sentenceToFeatures(text)
         arousal_docs.append((txt, a))
     except:
         print("Failed to add text: ", text)
 
 print("Building training sets...")
+# Convert list of docs to dictionary format required by nltk classifier
 arousal_training_set = [({word: (True) for word in x[0]}, x[1]) for x in arousal_docs]
 
 print("Training Arousal Classifier...")
+# Train classifier using naive bayes from nltk
 arousal_classifier = NaiveBayesClassifier.train(arousal_training_set)
 
+# Using nltk valence classifier, VADER. Trained on data from twitter.
 vader = SentimentIntensityAnalyzer()
 
 print("Testing...")
 
 def analyzeSentiment(sentence):
-    #test_data_features = {word: (word in sentenceToFeatures(sentence)) for word in dictionary}
+    #convert string to format readable by classifier. Lemmatizes and removes stopwords as well.
     test_data_features = {word: True for word in sentenceToFeatures(sentence)}
-    vad = vader.polarity_scores(sentence)['compound']
+    # the valence score using the VADER classifier
+    valence = vader.polarity_scores(sentence)['compound']
+    # arousal comes from the model trained in this program
     arousal = arousal_classifier.classify(test_data_features)
-    return categorizeSentiment(vad, arousal)
+    return categorizeSentiment(valence, arousal)
 
 
-# list of tuples (text, sentiment, phonemes)
+# annotated text: list of tuples (text, sentiment, phonemes)
 annotated_text = []
+
+"""
+    Classify sentiment for all sentences in testData, as well as count the phonemes.
+    Result is stored in tuple in annotated_text
+"""
 for sentence in testData:
     sentiment = analyzeSentiment(sentence)
     phonemeCount = countPhonemes(sentence)
     annotated_text.append((sentence, sentiment, phonemeCount))
+
+# printing annotated result to console.
 for entry in annotated_text:
     print(entry)
